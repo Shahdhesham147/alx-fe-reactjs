4073d45fcad9b59d738b6d9dd6e76cab1812adfd
Shahd <shahdhisham047@gmail.com>
2025-03-26 19:42:31 +0200

    Initial commit - Reset everything

---
+++ src/MainContent.jsx
@@ -0,0 +1,6 @@
+function Header() {
+    return <header><h1>Welcome to My App</h1></header>;
+  }
+  
+  export default Header;
+  
---
+++ srcFooter.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return <header><h1>Welcome to My App</h1></header>;
+  }
+  
+  export default Header;
+  
+const Header = () => {
+  return <header>Header Component</header>;
+};
+export default Header;
