c1d5a5d93ba715377b6f077fd7d6f63154ff4a34
Shahd <shahdhisham047@gmail.com>
2025-04-08 23:45:28 +0200

    Initial commit for Recipe Sharing App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// App.jsx
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🍽️ Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
